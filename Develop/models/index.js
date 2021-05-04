95f08b02e7952ae80934bc6a9804ad06f647cc16
Kevin Jun <kwjun90@gmail.com>
2021-05-04 16:57:27 -0500

    completed category.js and product.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,18 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {foreignKey:"catergory_id"});
 
 // Categories have many Products
+Category.hasMany(Product, {foreignKey:"catergory_id"});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(tag, {through: ProductTag, foreignKey:"product_id"});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {through: ProductTag, foreignKey:"tag_id"});
+
+
 
 module.exports = {
   Product,
